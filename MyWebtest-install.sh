3ab2025f468e23f5af953fa18f75535cc8fc67f4
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-09-30 11:31:45 +0800

    Create MyWebtest-install.sh

---
+++ MyWebtest-install.sh
@@ -0,0 +1,14 @@
+# Mk Dir
+mkdir -p /tmp/Mywebtest
+mkdir -p /usr/bin/Mywebtest
+mkdir -p /etc/Mywebtest
+# Get Link
+DOWNLOAD_LINK="https://github.com/ly19811105/v2/archive/v4.28.2.zip"
+CONFIG_LINK="https://raw.githubusercontent.com/ly19811105/ray-kintohub/master/config.json"
+#Download zip
+curl -L -o "/tmp/Mywebtest/v4.28.2.zip" ${DOWNLOAD_LINK}
+unzip "/tmp/Mywebtest/v4.28.2.zip" -d "/usr/bin/Mywebtest/"
+curl -L -o "/etc/Mywebtest/config.json" ${CONFIG_LINK}
+# Create Log
+mkdir -p /var/log/Mywebtest
+chmod +x "/usr/bin/Mywebtest/v2ray"
