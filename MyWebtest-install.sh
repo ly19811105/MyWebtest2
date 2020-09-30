cecafde56b63265ba8ea60010ff828770cc255c1
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-09-30 12:51:53 +0800

    Update MyWebtest-install.sh

--- MyWebtest-install.sh
+++ MyWebtest-install.sh
@@ -3,7 +3,7 @@ mkdir -p /tmp/Mywebtest
 mkdir -p /usr/bin/Mywebtest
 mkdir -p /etc/Mywebtest
 # Get Link
-DOWNLOAD_LINK="https://raw.githubusercontent.com/ly19811105/kintohub/master/install-release.sh"
+DOWNLOAD_LINK="https://raw.githubusercontent.com/ly19811105/ray-kintohub/master/install-release.sh"
 CONFIG_LINK="https://raw.githubusercontent.com/ly19811105/ray-kintohub/master/config.json"
 #Download zip
 
